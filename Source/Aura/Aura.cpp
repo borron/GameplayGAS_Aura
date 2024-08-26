0437ada09791b77dfcc07a0182b185b88e326bc7
Ronnie Bornerot <ronnie.bornerot@gmail.com>
2024-08-27 00:50:50 +0200

    Sect. 2, Lect. 5; The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Brisingamen
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Brisingamen
+
+#pragma once
+
+#include "CoreMinimal.h"
+
